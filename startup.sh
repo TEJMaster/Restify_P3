5f7259782063e8bdb73298d68da75f2f71ba1d37
TEJMaster <69652431+TEJMaster@users.noreply.github.com>
2023-04-19 00:53:52 -0400

    setup

--- startup.sh
+++ startup.sh
@@ -2,5 +2,5 @@ pip3 install virtualenv
 virtualenv -p /usr/bin/python3 venv  
 source venv/bin/activate  
 pip3 install -r requirements.txt  
-python3 ./restify/restify/manage.py makemigrations  
-python3 ./restify/restify/manage.py migrate
+python3 ./backend/manage.py makemigrations  
+python3 ./backend/manage.py migrate
