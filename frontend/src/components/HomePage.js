3349c726d7503afd56375b6b8b5c110f154d44ae
TEJMaster <69652431+TEJMaster@users.noreply.github.com>
2023-04-19 15:48:00 -0400

    home page

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import './HomePage.css'; // You'll create this file in the next step
+import './HomePage.css';
+import logo from './images/logo.png'; // Import the logo
+import videoSource from './images/Video.mp4'; // Import the video source
 
 const HomePage = () => {
   return (
     <div className="index_head">
-      <video autoplay loop muted playsInline className="back-video">
-        <source src="images/Video.mp4" type="video/mp4" />
+      <video autoPlay loop muted playsInline className="back-video">
+        <source src={videoSource} type="video/mp4" />
       </video>
 
       <nav>
-        <img src="images/logo.png" className="logo" alt="logo" />
+        <img src={logo} className="logo" alt="logo" />
         <ul>
           <li>
             <a href="home.html">HOME</a>
@@ -18,14 +20,15 @@ const HomePage = () => {
             <a href="signup.html">SIGN UP</a>
           </li>
           <li>
-            <a href="login.html">LOG IN</a>
+            <a href="signin.html">SIGN IN</a>
           </li>
         </ul>
       </nav>
+
       <div className="content">
-        <h1>Restify</h1>
-        <a href="home.html">
-          <span></span>Explore your place
+        <h1>WELCOME</h1>
+        <a href="properties.html">
+          <span></span>SEARCH PROPERTIES
         </a>
       </div>
     </div>
