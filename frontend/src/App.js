2e023ca547642d156d04ae4b011f8cbac18c7000
TEJMaster <69652431+TEJMaster@users.noreply.github.com>
2023-04-20 23:36:02 -0400

    a

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import HomePage from './components/homepage';
+import HomePage from './components/homepages';
 import SignUpPage from './components/signuppage';
 import LoginPage from './components/loginpage';
 
