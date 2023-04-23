54ad508811bc0a8b6caf66134c4585f3426e96ef
Houyi Chen <houyi.chen@mail.utoronto.ca>
2023-04-23 00:55:52 -0400

    client reservation

--- frontend/src/components/reservationpage.js
+++ frontend/src/components/reservationpage.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './css/ReservationPage.css';
 import './css/NavBar.css';
+import NavBar from './navbar';
+import { Link } from 'react-router-dom';
+
 
 const ReservationPage = () => {
     const [reservations, setReservations] = useState([]);
@@ -12,68 +15,114 @@ const ReservationPage = () => {
     const [userId, setUserId] = useState(null);
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
-    const [selectedAmenities, setSelectedAmenities] = useState([]);
-    const [tempSelectedAmenities, setTempSelectedAmenities] = useState([]);
+
+    const [propertyIdToCancel, setPropertyIdToCancel] = useState(null);
+    const [selectedState, setSelectedState] = useState('');
+    const [houseimage, setHouseImage] = useState('');
 
 
 
-    function handleAmenityChange(e) {
-      const amenityId = parseInt(e.target.value);
-      if (e.target.checked) {
-        setTempSelectedAmenities([...tempSelectedAmenities, amenityId]);
-      } else {
-        setTempSelectedAmenities(tempSelectedAmenities.filter((id) => id !== amenityId));
-      }
+
+
+    function handleStateFilterChange(e) {
+      setSelectedState(e.target.value);
     }
     
-    function handleFilterConfirm() {
-      setSelectedAmenities(tempSelectedAmenities);
-    }
 
-    const fetchReservations = async (page = 1) => {
-      try {
-        const response = await axios.get(`http://localhost:8000/reservation/?amenities=${selectedAmenities.join(',')}&page=${page}`);
+    // const fetchReservations = async (page = 1) => {
+    //   try {
+    //     const response = await axios.get(`http://localhost:8000/reservation/?amenities=${selectedAmenities.join(',')}&page=${page}`);
   
-        // Handle successful fetch
-        if (response.status === 200) {
+    //     // Handle successful fetch
+    //     if (response.status === 200) {
 
-          // const userReservations = response.data.results.filter(
-          //   (reservation) => reservation.user === userId
-          // );
+    //       // const userReservations = response.data.results.filter(
+    //       //   (reservation) => reservation.user === userId
+    //       // );
 
-          const userReservations = response.data.results;
-          setReservations(userReservations);
-          setTotalPages(Math.ceil(response.data.count / response.data.page_size));
-        }
-      } catch (error) {
-        setError('Error fetching reservations');
-      }
-    };
+    //       const userReservations = response.data.results;
+    //       setReservations(userReservations);
+    //       setTotalPages(Math.ceil(response.data.count / response.data.page_size));
+    //     }
+    //   } catch (error) {
+    //     setError('Error fetching reservations');
+    //   }
+    // };
   
+    // useEffect(() => {
+    //   fetchReservations();
+    // }, [selectedAmenities]);
+
     useEffect(() => {
-      fetchReservations();
-    }, [selectedAmenities]);
+      const fetchReservations = async () => {
+        try {
+          const token = localStorage.getItem('access_token');
+          const headers = { Authorization: `Bearer ${token}` };
+          const response = await axios.get('http://localhost:8000/reservation/', { headers });
     
-    function handleCancelClick(reservationId) {
+          const tokenParts = token.split('.');
+          const tokenPayload = tokenParts[1];
+          const tokenPayloadJson = atob(tokenPayload);
+          const tokenPayloadArray = JSON.parse(tokenPayloadJson);
+    
+          const UserId = tokenPayloadArray.user_id;
+
+          
+          
+
+    
+          let userReservations = response.data.results
+            .filter((reservation) => reservation.user === UserId)
+            .map((reservation) => {
+          return {
+            ...reservation,
+            property: {
+            ...reservation.property,
+            image: `http://localhost:8000/property_images${response.data.image}`,
+            },
+          };
+          });
+
+    
+          if (selectedState) {
+            userReservations = userReservations.filter(
+              (reservation) => reservation.state === selectedState
+            );
+          }
+    
+          setReservations(userReservations);
+        } catch (error) {
+          console.error('Error fetching reservations:', error);
+        }
+      };
+    
+      fetchReservations();
+    }, [selectedState]);
+    
+    
+    function handleCancelClick(reservationId, propertyId) {
       setReservationIdToCancel(reservationId);
+      setPropertyIdToCancel(propertyId);
       setShowConfirmation(true);
     }
+    
 
     async function handleCancelConfirmation(reservationId) {
       try {
-        const response = await axios.get(
-          `http://localhost:8000/reservation/cancel/${reservationId}/`
-        );
+        const token = localStorage.getItem('access_token');
+        const headers = { Authorization: `Bearer ${token}` };
+        const response = await axios.put(`http://localhost:8000/reservation/cancel/${reservationId}/`, { property: propertyIdToCancel }, { headers });
     
         if (response.status === 200) {
           setShowConfirmation(false);
           setShowCancellationSubmitted(true);
         }
       } catch (error) {
-        console.error("Error canceling reservation:", error);
+        console.error("Error canceling reservation:", error.response.data);
       }
-      
     }
+    
+    
 
     function ConfirmPopup({ message, reservationId, onYes, onClose }) {
       return (
@@ -98,33 +147,62 @@ const ReservationPage = () => {
       );
     }
 
-    function handlePaginationClick(newPage) {
-      setCurrentPage(newPage);
-      fetchReservations(newPage);
+    // function handlePaginationClick(newPage) {
+    //   setCurrentPage(newPage);
+    //   fetchReservations(newPage);
+    // }
+  
+    function ReservationFilter({ selectedState, handleStateFilterChange }) {
+      return (
+        <div className="reservation-filter">
+          <label htmlFor="state-filter">Filter by state: </label>
+          <select
+            id="state-filter"
+            value={selectedState}
+            onChange={handleStateFilterChange}
+          >
+            <option value="">All</option>
+            <option value="Pending">Pending</option>
+            <option value="Approved">Approved</option>
+            <option value="Denied">Denied</option>
+            <option value="Canceled">Canceled</option>
+            <option value="Terminated">Terminated</option>
+            <option value="Completed">Completed</option>
+            <option value="Expired">Expired</option>
+            <option value="Pending Cancel">Pending Cancel</option>
+          </select>
+        </div>
+      );
     }
+    
 
+  
   return (
     
     <div className="reservation-page">
-            <nav className="navbar">
-              <a href="/">Home</a>
-              <a href="/reservations">My Reservations</a>
-            </nav>
+            <NavBar />
         <section className="banner">
         <div className="banner-image"></div>
         <div className="banner-text">
             <h1>Find your perfect stay</h1>
             <p>Explore our collection of amazing properties.</p>
-            <a href="stays.html" className="button">
-                Get started
-            </a>
+            <Link to="/logged_main" className="button">
+              Get started
+            </Link>
+
   </div>
         </section>
         <section className="my-stays">
         <h2>My Reservations</h2>
+        <ReservationFilter
+        selectedState={selectedState}
+        handleStateFilterChange={handleStateFilterChange}
+      />
         {reservations.map((reservation) => (
       <div key={reservation.id} className="stay">
         <img src={reservation.property.image} alt={reservation.property.name} className="stay-image" />
+
+
         <div className="stay-details">
           <h3>{reservation.property.name}</h3>
           <p>
@@ -135,39 +213,12 @@ const ReservationPage = () => {
           </p>
         </div>
           <div className="button-state-container">
-            <button className="cancel-button" onClick={() => handleCancelClick(reservation.id)}>Cancel</button>
+          <button className="cancel-button" onClick={() => handleCancelClick(reservation.id, reservation.property.id)}>Cancel</button>
+
             <p className="reservation-state">{reservation.state}</p>
           </div>
 
-          <section className="filter-bar">
-      <div className="ticks">
-        <h2>Filter</h2>
-        <h3>Property Type</h3>
-        <div className="filter">
-          <input type="checkbox" /> <p>House</p>
-        </div>
-        <div className="filter">
-          <input type="checkbox" /> <p>Hostel</p>
-        </div>
-        <div className="filter">
-          <input type="checkbox" /> <p>Condo</p>
-        </div>
-
-        <h3>Amenities</h3>
-        <div className="filter">
-          <input type="checkbox" /> <p>Wifi</p>
-        </div>
-        <div className="filter">
-          <input type="checkbox" /> <p>Tub</p>
-        </div>
-        <div className="filter">
-          <input type="checkbox" /> <p>Balcony</p>
-        </div>
-        <div className="filter">
-          <input type="checkbox" /> <p>Kitchen</p>
-        </div>
-      </div>
-    </section>
+          
 
         {showConfirmation && (
             <ConfirmPopup
@@ -192,7 +243,7 @@ const ReservationPage = () => {
       
 
 
-      <div className="pagination">
+      {/* <div className="pagination">
       {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
         <button
           key={page}
@@ -202,7 +253,7 @@ const ReservationPage = () => {
           {page}
         </button>
       ))}
-    </div>
+    </div> */}
     </div>
 
     
