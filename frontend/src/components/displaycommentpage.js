329e8cc84f0478a1bae641d90bf7d4757b287b9e
TEJMaster <69652431+TEJMaster@users.noreply.github.com>
2023-04-24 14:48:17 -0400

    pagination

--- frontend/src/components/displaycommentpage.js
+++ frontend/src/components/displaycommentpage.js
@@ -7,60 +7,85 @@ import NavBar from './navbar';
 const DisplayCommentsPage = () => {
   const { target_username } = useParams(); // Extract the target username from the URL
   const [comments, setComments] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [nextPage, setNextPage] = useState(null);
+  const [prevPage, setPrevPage] = useState(null);
 
   useEffect(() => {
-    const fetchComments = async () => {
+    const fetchComments = async (page) => {
       try {
         const token = localStorage.getItem('access_token');
         const headers = { Authorization: `Bearer ${token}` };
-  
+
         const response = await axios.get(
-          `http://localhost:8000/comments/view/user/${target_username}`,
+          `http://localhost:8000/comments/view/user/${target_username}?page=${page}`,
           { headers } // Pass the headers object with the request
         );
-  
+
         if (response.status === 200) {
           setComments(response.data.results);
+          setNextPage(response.data.next);
+          setPrevPage(response.data.previous);
           console.log('Comments fetched:', response.data.results);
         }
       } catch (error) {
         console.error('Error fetching comments:', error);
       }
     };
-  
-    fetchComments();
-  }, [target_username]);
-  
-  
+
+    fetchComments(currentPage);
+  }, [target_username, currentPage]);
+
+  const handleNextPage = () => {
+    if (nextPage) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (prevPage) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
 
   return (
     <>
-    <NavBar />
-    <div className="display-comments-page">
-      <h2>Comments for {target_username}:</h2>
-      {comments.length > 0 ? (
-        <ul className="comments-list">
-          {comments.map((comment) => (
-            <li key={comment.id} className="comment-item">
-              <div className="comment-author">
-                <span> User: {comment.author.username}</span>
-              </div>
-              <div className="comment-content">
-                <p>Comment: {comment.content}</p>
-              </div>
-              <div className="comment-rating">
-                <span>Rating: {comment.rate} stars</span>
-              </div>
-              <div className="comment-date">
-                <span>Posted on: {new Date(comment.created_at).toLocaleDateString()}</span>
-              </div>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No comments found.</p>
-      )}
-    </div>
+      <NavBar />
+      <div className="display-comments-page">
+        <h2>Comments for {target_username}:</h2>
+        {comments.length > 0 ? (
+          <ul className="comments-list">
+            {comments.map((comment) => (
+              <li key={comment.id} className="comment-item">
+                <div className="comment-author">
+                  <span> User: {comment.author.username}</span>
+                </div>
+                <div className="comment-content">
+                  <p>Comment: {comment.content}</p>
+                </div>
+                <div className="comment-rating">
+                  <span>Rating: {comment.rate} stars</span>
+                </div>
+                <div className="comment-date">
+                  <span>
+                    Posted on: {new Date(comment.created_at).toLocaleDateString()}
+                  </span>
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No comments found.</p>
+        )}
+        <div className="pagination">
+          <button onClick={handlePrevPage} disabled={!prevPage}>
+            Previous
+          </button>
+          <button onClick={handleNextPage} disabled={!nextPage}>
+            Next
+          </button>
+        </div>
+      </div>
     </>
   );
 };
